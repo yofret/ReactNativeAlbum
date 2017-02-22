2ce93e2eee0f13b560bc045912b46d646ce0b5ee
Yofret Rios <yofret.rios@jobsity.com>
2017-02-22 11:21:20 -0500

    Styling my first header

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,35 @@
 // App Dependencies 
 //-------------------------------
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 //-------------------------------
 // Header Component
 //-------------------------------
 const Header = () => {
 
-	const { textStyle } = styles;
+	const { textStyle, viewStyle } = styles;
 
-	return <Text style={textStyle} >Albums</Text>;
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle} >Albums</Text>
+		</View>
+	);
 };
 
 const styles = {
+	viewStyle: {
+		backgroundColor: '#F8F8F8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.2,
+		elevation: 2,
+		position: 'relative'
+	},
 	textStyle: {
 		fontSize: 20
 	}
