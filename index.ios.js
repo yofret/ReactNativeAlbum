63255905f052ecc327eff8420f88f9ca46ab8bbe
Yofret Rios <yofret.rios@jobsity.com>
2017-02-23 11:25:51 -0500

    Adding our first list component

--- index.ios.js
+++ index.ios.js
@@ -5,19 +5,19 @@
 	import React from 'react';
 	import { AppRegistry, View } from 'react-native';
 	import Header from './src/components/Header';
-	import AlbumList from './src/components/AlbumList'
+	import AlbumList from './src/components/AlbumList';
 
 //-------------------------------
 // Header Component
 //-------------------------------
 
 	const App = () => {
-	    return (
-	    	<View>
-	    		<Header headerText={'Albums'}/>
-	    		<AlbumList />
-	    	</View>
-	    );
+		return (
+			<View>
+				<Header headerText={'Albums'} />
+				<AlbumList />
+			</View>
+		);
 	};
 
 //-------------------------------
