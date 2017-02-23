219f8fa0a9497d88ae49f53fc1d4348ab76e4334
Yofret Rios <yofret.rios@jobsity.com>
2017-02-23 12:26:20 -0500

    Defining style for card section

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,17 +3,35 @@
 //-------------------------------
 
 	import React from 'react';
-	import { Text } from 'react-native';
+	import { View } from 'react-native';
 
 //-------------------------------
 // Card Component
 //-------------------------------
 
 	const Card = (props) => {
-
 		return (
-			<View></View>
+			<View style={styles.containerStyle}>
+				{props.children}
+			</View>
 		);
+	};
+
+	const styles = {
+		containerStyle: {
+			borderWidth: 1,
+			borderRadius: 2,
+			borderColor: '#DDD',
+			borderBottomWidth: 0,
+			shadowColor: '#000',
+			shadowOffset: { width: 0, height: 2 },
+			shadowOpacity: 0.1,
+			shadowRadius: 2,
+			elevation: 1,
+			marginLeft: 5,
+			marginRight: 5,
+			marginTop: 10
+		}
 	};
 
 //-------------------------------
