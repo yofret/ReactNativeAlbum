6182ed95c33827ed5d7e7d1a0e01fc075dd414e6
Yofret Rios <yofret.rios@jobsity.com>
2017-02-23 11:26:21 -0500

    Changing from Functional component to Class based Component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,20 +2,43 @@
 // App Dependencies 
 //-------------------------------
 
-	import React from 'react';
-	import { View, Text } from 'react-native';
+	import React, { Component } from 'react';
+	import { View } from 'react-native';
+	import axios from 'axios';
+	import AlbumDetail from './AlbumDetail';
 
 //-------------------------------
 // AlbumList Component
 //-------------------------------
 
-	const AlbumList = () => {
-		return (
-			<View>
-				<Text>Album list!!!</Text>
-			</View>
-		);
-	};
+	class AlbumList extends Component {
+		//Component State
+		state = {
+			albums: []
+		}
+
+		//Life cycle methods
+		componentWillMount() {
+			axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+				.then(response => this.setState({ albums: response.data }));
+		}
+
+		// Render methods
+		renderAlbums() {
+			return this.state.albums.map(album => 
+				<AlbumDetail key={album.title} album={album} />
+			);
+		}
+
+		render() {
+			console.log(this.state.albums);
+			return (
+				<View>
+					{this.renderAlbums()}
+				</View>
+			);
+		}					
+	}
 
 //-------------------------------
 // Export Component
