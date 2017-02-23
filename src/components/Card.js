b5f18991db163e4a7c12b061415ab4ec839354fc
Yofret Rios <yofret.rios@jobsity.com>
2017-02-23 11:50:19 -0500

    Adding Card component

---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+//-------------------------------
+// App Dependencies 
+//-------------------------------
+
+	import React from 'react';
+	import { Text } from 'react-native';
+
+//-------------------------------
+// Card Component
+//-------------------------------
+
+	const Card = (props) => {
+
+		return (
+			<View></View>
+		);
+	};
+
+//-------------------------------
+// Export Component
+//-------------------------------
+	export default Card;
