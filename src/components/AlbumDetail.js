6a542cc7afb35f5a8ffd7f9cd4cff66a42683333
Yofret Rios <yofret.rios@jobsity.com>
2017-02-24 10:32:34 -0500

    Adding more styling to the our album detail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -12,19 +12,29 @@
 //-------------------------------
 
 	const AlbumDetail = ({ album }) => {
-
-		const { title, artist, thumbnail_image } = album;
+		// Destructuring our objects
+		const { title, artist, thumbnail_image, image } = album;
+		const { 
+			thumbnailStyle, 
+			headerContentStyle, 
+			thumbnailContainerStyle, 
+			headerTextStyle,
+			imageStyle 
+		} = styles;
 
 		return (
 			<Card>
 				<CardSection>
-					<View>
-						<Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+					<View style={thumbnailContainerStyle}>
+						<Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
 					</View>
-					<View style={styles.headerContentStyle}>
-						<Text>{title}</Text>
+					<View style={headerContentStyle}>
+						<Text style={headerTextStyle}>{title}</Text>
 						<Text>{artist}</Text>
 					</View>
+				</CardSection>
+				<CardSection>
+					<Image style={imageStyle} source={{ uri: image }} />
 				</CardSection>
 			</Card>
 		);
@@ -35,9 +45,23 @@
 			flexDirection: 'column',
 			justifyContent: 'space-around'
 		},
+		headerTextStyle: {
+			fontSize: 18
+		},
 		thumbnailStyle: {
 			width: 50,
 			height: 50
+		},
+		thumbnailContainerStyle: {
+			justifyContent: 'center',
+			alignItems: 'center',
+			marginLeft: 10,
+			marginRight: 10
+		},
+		imageStyle: {
+			height: 300,
+			flex: 1,
+			width: null
 		}
 	};
 
